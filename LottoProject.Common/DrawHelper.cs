ad2c2fc52158f6442f514401ab1cc844bd1260a4
mrmabaquiao <marvinrmabaquiao@gmail.com>
2015-11-04 21:29:08 +0800

    Added Create Model and Common Method

---
+++ LottoProject.Common/DrawHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoProject.Common
+{
+    public class DrawHelper
+    {
+        public static List<DrawCategory> GetDrawCategories()
+        {
+            var drawCategories = new List<DrawCategory>()
+            {
+                new DrawCategory(){Id = 1,CategoryName="642"},
+                new DrawCategory(){Id =2,CategoryName="645"},
+                new DrawCategory(){Id =3,CategoryName="649"},
+                new DrawCategory(){Id =4,CategoryName="655"},
+                new DrawCategory(){Id =5,CategoryName="658"}
+                
+
+            };
+            return drawCategories;
+        }
+
+    }
+
+    public class DrawCategory
+    {
+        public int Id {get;set;}
+        public string CategoryName { get; set; }
+       
+
+
+    }
+}
---
+++ LottoProject.Web/Models/DrawCreateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LottoProject.Web.Models
+{
+    public class DrawCreateModel
+    {
+        public virtual int Id { get; set; }
+        public virtual int FirstDigit { get; set; }
+        public virtual int SecondDigit { get; set; }
+        public virtual int ThirdDigit { get; set; }
+        public virtual int FourthDigit { get; set; }
+        public virtual int FifthDigit { get; set; }
+        public virtual int SixthDigit { get; set; }
+        public virtual string Catergory { get; set; }
+    }
+}
